4cd9ac8c2a57ee0810e6b6a1f44dbe6cd56048b8
pausanz <psppau@hotmail.com>
2017-02-13 17:58:45 +0100

    BdD amb Twig, fet

--- src/clinicaPaniBundle/Entity/Client.php
+++ src/clinicaPaniBundle/Entity/Client.php
@@ -8,74 +8,34 @@ use Doctrine\ORM\Mapping as ORM;
  * Client
  *
  * @ORM\Table(name="client")
- * @ORM\Entity(repositoryClass="clinicaPaniBundle\Repository\ClientRepository")
+ * @ORM\Entity
  */
 class Client
 {
     /**
-     * @var int
-     *
-     * @ORM\Column(name="id", type="integer")
-     * @ORM\Id
-     * @ORM\GeneratedValue(strategy="AUTO")
-     */
-    private $id;
-
-    /**
      * @var string
      *
-     * @ORM\Column(name="dni", type="string", length=255, unique=true)
-     */
-    private $dni;
-
-    /**
-     * @var string
-     *
-     * @ORM\Column(name="nom", type="string", length=255)
+     * @ORM\Column(name="nom", type="string", length=255, nullable=false)
      */
     private $nom;
 
     /**
      * @var string
      *
-     * @ORM\Column(name="cognom", type="string", length=255)
+     * @ORM\Column(name="cognom", type="string", length=255, nullable=false)
      */
     private $cognom;
 
-
     /**
-     * Get id
+     * @var string
      *
-     * @return int
+     * @ORM\Column(name="dni", type="string", length=9)
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="IDENTITY")
      */
-    public function getId()
-    {
-        return $this->id;
-    }
+    private $dni;
 
-    /**
-     * Set dni
-     *
-     * @param string $dni
-     *
-     * @return Client
-     */
-    public function setDni($dni)
-    {
-        $this->dni = $dni;
 
-        return $this;
-    }
-
-    /**
-     * Get dni
-     *
-     * @return string
-     */
-    public function getDni()
-    {
-        return $this->dni;
-    }
 
     /**
      * Set nom
@@ -124,5 +84,14 @@ class Client
     {
         return $this->cognom;
     }
-}
 
+    /**
+     * Get dni
+     *
+     * @return string
+     */
+    public function getDni()
+    {
+        return $this->dni;
+    }
+}
