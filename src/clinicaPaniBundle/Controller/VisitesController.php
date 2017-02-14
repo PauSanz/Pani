c5dcc3c6af34c752631e87ac3a03cf2e019d3097
pausanz <psppau@hotmail.com>
2017-02-14 17:29:54 +0100

    Veure Detalls Visita

--- src/clinicaPaniBundle/Controller/VisitesController.php
+++ src/clinicaPaniBundle/Controller/VisitesController.php
@@ -14,4 +14,26 @@ class VisitesController extends Controller {
         ));
     }
 
+    public function veureDetallsAction($ref) {
+        //return $this->render('clinicaPaniBundle:Default:index.html.twig');
+        $visita = $this->getDoctrine()->getRepository('clinicaPaniBundle:Visita')->findOneBy(array('ref' => $ref));
+
+        if (!$visita) {
+            /* throw $this->createNotFoundException(
+              'No s\'ha trobat categoría per la id '.$id
+              ); */
+            $response = $this->render('clinicaPaniBundle:Default:404.html.twig', array(
+                'message' => 'No s\'ha trobat cap visita ' . $ref
+            ));
+
+            $response->setStatusCode(404);
+
+            return $response;
+        }
+
+        return $this->render('clinicaPaniBundle:Default:dtllsvisita.html.twig', array(
+                    'ref' => 'Número de referència ' . $ref,
+                    'visita' => $visita));
+    }
+
 }
