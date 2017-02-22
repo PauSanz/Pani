8c51ecb2259627f60a3c5003e1a88129b6d8c307
pausanz <psppau@hotmail.com>
2017-02-22 17:25:20 +0100

    Vistes de Metges i Visites
    CRUD DE METGES, OK.
    CRUD DE VISITES, PROBLEMS AFEGIR

--- src/clinicaPaniBundle/Controller/VisitesController.php
+++ src/clinicaPaniBundle/Controller/VisitesController.php
@@ -3,11 +3,14 @@
 namespace clinicaPaniBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use clinicaPaniBundle\Entity\Visita;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 
 class VisitesController extends Controller {
 
     public function vistaVisitaAction() {
-        //return $this->render('clinicaPaniBundle:Default:index.html.twig');
         $visites = $this->getDoctrine()->getRepository('clinicaPaniBundle:Visita')->findAll();
         return $this->render('clinicaPaniBundle:Default:vvisites.html.twig', array(
                     'Visites' => $visites,
@@ -16,15 +19,11 @@ class VisitesController extends Controller {
     }
 
     public function veureDetallsAction($ref) {
-        //return $this->render('clinicaPaniBundle:Default:index.html.twig');
         $visita = $this->getDoctrine()->getRepository('clinicaPaniBundle:Visita')->findOneBy(array('ref' => $ref));
 
         if (!$visita) {
-            /* throw $this->createNotFoundException(
-              'No s\'ha trobat categoría per la id '.$id
-              ); */
             $response = $this->render('clinicaPaniBundle:Default:404.html.twig', array(
-                'message' => 'No s\'ha trobat cap visita ' . $ref
+                'message' => 'No s\'ha trobat cap visita amb el número de referència' . $ref
             ));
 
             $response->setStatusCode(404);
@@ -33,8 +32,109 @@ class VisitesController extends Controller {
         }
 
         return $this->render('clinicaPaniBundle:Default:dtllsvisita.html.twig', array(
-                    'titol' => 'Detalls de la visita ref: ' . $ref,
+                    'titol' => 'Detalls de la visita referència núm: ' . $ref,
                     'visita' => $visita));
+    }
+
+    public function afegirVisitaAction(Request $req) {
+        $visita = new Visita();
+        $form = $this->createFormBuilder($visita)
+                ->add('ref', TextType::class, array('label' => 'Referència'))
+                ->add('data', TextType::class, array('label' => 'Data'))
+                ->add('hora', TextType::class, array('label' => 'Hora'))
+                ->add('descripcio', TextType::class, array('label' => 'Descripció'))
+                //APLICAR DROPDOWN CHOICE
+                ->add('tipusVisita', TextType::class, array('label' => 'Tipus'))
+                ->add('metgeVisitat', TextType::class, array('label' => 'Metge'))
+                ->add('pacientVisitat', TextType::class, array('label' => 'Pacient'))
+                ->add('afegir', SubmitType::class, array('label' => 'Afegir Visita'))
+                ->getForm();
+
+        $form->handleRequest($req);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $visita = $form->getData();
+
+            $em = $this->getDoctrine()->getManager();
+            $em->persist($visita);
+            $em->flush();
+
+            return $this->redirectToRoute('clinica_pani_vistavisites');
+        }
+        return $this->render('clinicaPaniBundle:Default:avisita.html.twig', array(
+                    'titol' => 'Afegir Metge',
+                    'form' => $form->createView()));
+        //}
+    }
+
+    public function modificarVisitaAction($ref, Request $req) {
+
+        $em = $this->getDoctrine()->getEntityManager();
+        $visites = $em->getRepository("clinicaPaniBundle:Visita");
+
+        $visita = $visites->findOneBy(array('ref' => $ref));
+
+        if (!$visita) {
+
+            $response = $this->render('clinicaPaniBundle:Default:404.html.twig', array(
+                'message' => 'No s\'ha pogut trobar la visita, visita no existent '
+            ));
+
+            $response->setStatusCode(404);
+
+            return $response;
+        } else {
+            $form = $this->createFormBuilder($visita)
+                    ->add('ref', TextType::class, array('label' => 'Referència', 'data' => $visita->getRef()))
+                    ->add('data', TextType::class, array('label' => 'Data', 'data' => $visita->getData()))
+                    ->add('hora', TextType::class, array('label' => 'Hora', 'data' => $visita->getHora()))
+                    ->add('descripcio', TextType::class, array('label' => 'Descripció', 'data' => $visita->getDescripcio()))
+                    //APLICAR DROPDOWN CHOICE
+                    ->add('tipusVisita', TextType::class, array('label' => 'Tipus', 'data' => $visita->getTipusvisita()))
+                    ->add('metgeVisitat', TextType::class, array('label' => 'Metge', 'data' => $visita->getMetgevisitat()))
+                    ->add('pacientVisitat', TextType::class, array('label' => 'Pacient', 'data' => $visita->getPacientvisitat()))
+                    ->add('modificar', SubmitType::class, array('label' => 'Modificar Visita'))
+                    ->getForm();
+
+            $form->handleRequest($req);
+
+            if ($form->isSubmitted() && $form->isValid()) {
+                $visita = $form->getData();
+
+                $em = $this->getDoctrine()->getManager();
+                $em->persist($visita);
+                $em->flush();
+
+                return $this->redirectToRoute('clinica_pani_vistavisites');
+            }
+        }
+
+        return $this->render('clinicaPaniBundle:Default:mvisita.html.twig', array(
+                    'titol' => 'Modificar Visita',
+                    'form' => $form->createView()));
+    }
+
+    public function eliminarVisitaAction($ref) {
+
+        $em = $this->getDoctrine()->getEntityManager();
+        $visites = $em->getRepository("clinicaPaniBundle:Visita");
+
+        $visita = $visites->findOneBy(array('ref' => $ref));
+
+        if (!$visita) {
+            $response = $this->render('clinicaPaniBundle:Default:404.html.twig', array(
+                'message' => 'No s\'ha pogut essborrar la visita, visita no existent '
+            ));
+
+            $response->setStatusCode(404);
+
+            return $response;
+        } else {
+            $em->remove($visita);
+            $em->flush();
+            return $this->redirectToRoute('clinica_pani_vistavisites');
+        }
+        die();
     }
 
 }
